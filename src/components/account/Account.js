466c4f22c642f146e06951d30bc4e2a2e60f8cd5
Miroslav <derevianko_miroslav@ukr.net>
2018-06-03 15:28:42 +0300

    Change Register form, add footer and new header

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -4,7 +4,7 @@ import './Account.css';
 // connect component to redux
 import { connect } from 'react-redux';
 
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import logo from '../logo.jpg';
 
 //Import components
@@ -18,20 +18,34 @@ class Account extends Component {
   }
   render() {
     return (
-      <div>
-        <header className="Home-header">
-          <nav className="Home-navbar">
-            <div className="Home-brand">
-              <img src={logo} className="Home-logo" alt="logo" />
-              <h1 className="Home-title">Site Title</h1>
+      <div className="Account">
+        <header className="Account-header">
+          <div className="Account-navbar">
+            <div className="Account-brand">
+              <img src={logo} className="Account-logo" alt="logo" />
+              <h1 className="Account-title">Site Title</h1>
             </div>
-            <Langselect />
-            <Link className='btn btn-outline-light Home-link' to={'/registration'}>Log in</Link>
-            <Link className='btn btn-outline-light Home-link' to={'/account/register'}>Sign in</Link>
-          </nav>
+            <div className="Account-button-container">
+              <Langselect />
+              <div className="Account-button-link">
+                Token Sale
+              </div>
+            </div>
+          </div>
         </header>
-        <Route exact={true} path={"/account"} component={Profile}/>
-        <Route exact={true} path={"/account/register"} component={Register}/>
+        <Route exact={true} path={"/account"} component={Profile} />
+        <Route exact={true} path={"/account/register"} component={Register} />
+        <footer className="Account-footer">
+          <div className="Account-footer-container">
+            <div className="Account-footer-copyright">
+              @ 2018 GGC
+            </div>
+            <div className="Account-footer-link-container">
+              <a href="/" className="Account-footer-link">White book</a>
+              <a href="/" className="Account-footer-link">Token rules</a>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
